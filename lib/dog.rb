97eb630a947857ded5d31ea11ab87303115e6ada
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-21 00:52:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,15 +11,5 @@ class Dog
     @name
   end
 
-  def initialize(breed="Mutt")
-    @breed = breed
-  end
 
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end    
